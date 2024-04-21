37e591de5a3c43fa140e62e9d6cdc4f586c3165d
Yolohans <tomlegel@gmail.com>
2024-04-21 14:42:34 +0200

    make aggregate anbauplan jakarta.persistence ready

--- domain/src/main/java/de/dhbw/softwareengineering/anbauplaner/domain/anbauplan/Anbauplan.java
+++ domain/src/main/java/de/dhbw/softwareengineering/anbauplaner/domain/anbauplan/Anbauplan.java
@@ -1,25 +1,79 @@
 package de.dhbw.softwareengineering.anbauplaner.domain.anbauplan;
 
 import de.dhbw.softwareengineering.anbauplaner.domain.genericValueObjects.Name;
-import de.dhbw.softwareengineering.anbauplaner.domain.genericValueObjects.NameAttributeConverter;
+import de.dhbw.softwareengineering.anbauplaner.domain.genericValueObjects.converters.NameAttributeConverter;
 import de.dhbw.softwareengineering.anbauplaner.domain.genericValueObjects.Year;
-import de.dhbw.softwareengineering.anbauplaner.domain.genericValueObjects.YearAttributeConverter;
-import jakarta.persistence.Basic;
-import jakarta.persistence.Convert;
-import jakarta.persistence.Id;
+import de.dhbw.softwareengineering.anbauplaner.domain.genericValueObjects.converters.YearAttributeConverter;
+import jakarta.persistence.*;
 
 import java.util.List;
 
+@Entity
 public class Anbauplan {
     @Id
     private long id;
+
     @Convert(converter = NameAttributeConverter.class)
     private Name name;
+
     @Basic
     private boolean productive;
+
     @Convert(converter = YearAttributeConverter.class)
     private Year businessYear;
 
+    @OneToMany(mappedBy = "anbauplan")
     private List<Acker> ackers;
 
+    protected Anbauplan() {}
+
+    public Anbauplan(Name name, boolean productive, Year businessYear) {
+        this.name = name;
+        this.productive = productive;
+        this.businessYear = businessYear;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public boolean isProductive() {
+        return productive;
+    }
+
+    public Year getBusinessYear() {
+        return businessYear;
+    }
+
+    public List<Acker> getAckers() {
+        return ackers;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    public void setProductive(boolean productive) {
+        this.productive = productive;
+    }
+
+    public void setBusinessYear(Year businessYear) {
+        this.businessYear = businessYear;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Anbauplan{");
+        sb.append("id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", productive=").append(productive);
+        sb.append(", businessYear=").append(businessYear);
+        sb.append(", ackers=").append(ackers);
+        sb.append('}');
+        return sb.toString();
+    }
 }
--- domain/src/main/java/de/dhbw/softwareengineering/anbauplaner/domain/anbauplan/Belegung.java
+++ domain/src/main/java/de/dhbw/softwareengineering/anbauplaner/domain/anbauplan/Belegung.java
@@ -1,26 +1,71 @@
 package de.dhbw.softwareengineering.anbauplaner.domain.anbauplan;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import de.dhbw.softwareengineering.anbauplaner.domain.frucht.Frucht;
+import jakarta.persistence.*;
+import org.apache.commons.lang3.Validate;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Table(name = "belegung")
 public class Belegung {
     @Id
-    private String id;
+    @GeneratedValue
+    private UUID id;
 
-
-    public Belegung(String id, String name) {
-        this.id = id;
-    }
+    private LocalDate beginn;
+    private LocalDate ende;
+    @OneToOne
+    private Frucht frucht;
+    @OneToMany
+    private List<Anbauhinweis> anbauhinweise;
 
     protected Belegung() {
+        //for jakarta.persistence
+    }
+    public Belegung(LocalDate beginn, LocalDate ende, Frucht frucht, List<Anbauhinweis> anbauhinweise) {
+        Validate.isTrue(beginn.isBefore(ende), "'Beginn' must be before 'ende'");
+        this.beginn = beginn;
+        this.ende = ende;
+        this.frucht = frucht;
+        this.anbauhinweise = anbauhinweise;
     }
 
-    public String getId() {
+    protected UUID getId() {
         return id;
     }
 
+    protected LocalDate getBeginn() {
+        return beginn;
+    }
+
+    protected LocalDate getEnde() {
+        return ende;
+    }
+
+    protected Frucht getFrucht() {
+        return frucht;
+    }
+
+    protected List<Anbauhinweis> getAnbauhinweise() {
+        return anbauhinweise;
+    }
+
+    protected void setBeginn(LocalDate beginn) {
+        this.beginn = beginn;
+    }
+
+    protected void setEnde(LocalDate ende) {
+        this.ende = ende;
+    }
+
+    protected void setFrucht(Frucht frucht) {
+        this.frucht = frucht;
+    }
+
+    protected void setAnbauhinweise(List<Anbauhinweis> anbauhinweise) {
+        this.anbauhinweise = anbauhinweise;
+    }
 }
